befe9c7ba7eb4be28608d5dcea6d14c3957c0805
prono69 <faltu9851@gmail.com>
2025-03-04 19:39:41 +0530

    update

--- yt_dlp_plugins/extractor/missav.py
+++ yt_dlp_plugins/extractor/missav.py
@@ -1,12 +1,12 @@
 import re
-
+ 
 from yt_dlp.extractor.common import InfoExtractor
-
-
+ 
+ 
 class MissAVIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?missav\.com/.*/(?P<id>[\w-]+)'
+    _VALID_URL = r'https?://(?:www\.)?missav\.ws/.*/(?P<id>[\w-]+)'
     _TESTS = [{
-        'url': 'https://missav.com/en/blk-470-uncensored-leak',
+        'url': 'https://missav.ws/en/blk-470-uncensored-leak',
         'md5': 'f1537283a9bc073c31ff86ca35d9b2a6',
         'info_dict': {
             'id': 'blk-470-uncensored-leak',
@@ -14,10 +14,10 @@ class MissAVIE(InfoExtractor):
             'title': 'BLK-470 A Bitch Gal Who Seduces Her Best Friend\'s Boyfriend With A Micro Mini One Piece Dress - Eimi Fukada',
             'description': '',
             'thumbnail': r're:^https?://.*\.jpg$',
-
+ 
         },
     }]
-
+ 
     def _real_extract(self, url):
         
         video_id = self._match_id(url)
@@ -27,14 +27,14 @@ class MissAVIE(InfoExtractor):
         video_index = url_words.index("video")
         protocol = url_words[video_index-1]
         video_format = url_words[video_index + 1]
-
+ 
         m3u8_url_path = "-".join((url_words[0:5])[::-1])
         base_url_path = ".".join((url_words[5:video_index-1])[::-1])
-
+ 
         formatted_url = "{0}://{1}/{2}/{3}/{4}.m3u8".format(protocol, base_url_path, m3u8_url_path, video_format, url_words[video_index])
-
+ 
         self.to_screen('URL "%s" successfully captured' % formatted_url)
-
+ 
         formats = self._extract_m3u8_formats(formatted_url, video_id, 'mp4', m3u8_id='hls')
         
         
